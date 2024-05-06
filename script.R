d6eb79920e71ce54b6c8772f793a7e28cedfc0e0
slkelly22 <slkelly@go.olemiss.edu>
2024-05-05 19:58:26 -0500

    Added remaining content for Episode 2

--- script.R
+++ script.R
@@ -64,3 +64,85 @@ area_acres <- 2.47 * area_hectares
 # Then change area hectares to 50
 area_hectares <- 50
 # Exercise: What is the current content of area_acres -- 123.5 or 6.175
+
+# Exercise on creating objects
+r_length <- 7
+r_width <- 4
+area <- r_length * r_width
+area
+
+# Functions and their arguments
+b <- sqrt(a)
+
+round(3.14159)
+?round
+args(round)
+
+round(3.14159, 2)
+
+?cor
+args(cor)
+
+# Vectors and Data Types
+# Numeric vector
+hh_members <- c(3, 7, 10, 6)
+hh_members
+
+# Character vector
+respondent_wall_type <- c("muddaub", "burntbricks", "sunbricks")
+respondent_wall_type
+
+length(hh_members)
+length(respondent_wall_type)
+
+typeof(hh_members)
+typeof(respondent_wall_type)
+
+str(hh_members) # you get more info here than in typeof
+str(respondent_wall_type)
+
+# Adding elements to a vector
+possessions <- c("bicycle", "radio", "television")
+# Adding an element and overwriting the vector
+possessions <- c(possessions, "mobile_phone") # at the end
+possessions <- c("car", possessions) # at the beginning
+possessions
+
+# Subsetting Vectors
+respondent_wall_type
+respondent_wall_type[2]
+respondent_wall_type[c(3, 2)]
+more_respondent_wall_type <- respondent_wall_type[c(1, 2, 3, 2, 1, 3)]
+more_respondent_wall_type
+
+# Conditional Subsetting
+hh_members
+hh_members > 5
+hh_members[hh_members > 5]
+# Combine with Boolean
+hh_members[hh_members < 4 | hh_members > 7]
+hh_members[hh_members >= 4 & hh_members <= 7]
+# <, >, <=, >=, ==
+possessions
+possessions[possessions == "car" | possessions == "bicycle"]
+possessions == "car" | possessions == "bicycle"
+# %in% 
+possessions %in% c("car", "bicycle")
+possessions[possessions %in% c("car", "bicycle")]
+
+# Missing Data
+# In R, missing data is NA; functions na.rm = T
+rooms <- c(2, 1, 1, NA, 7)
+mean(rooms)
+mean(rooms, na.rm = T)
+max(rooms)
+max(rooms, na.rm = T)
+?is.na
+?na.omit
+?complete.cases
+# ! NOT operator
+rooms[!is.na(rooms)]
+sum(is.na(rooms))
+na.omit(rooms)
+rooms[complete.cases(rooms)]
+# Exercises
